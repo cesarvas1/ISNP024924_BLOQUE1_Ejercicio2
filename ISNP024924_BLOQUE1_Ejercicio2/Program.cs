4ca079dcae06c2b18b0a3f194d25a74486167d2b
César <Csar@DESKTOP-4INIV8U>
2024-10-20 18:11:59 -0600

    Agregar archivos de proyecto.

---
+++ ISNP024924_BLOQUE1_Ejercicio2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int numero;
+
+        do
+        {
+            Console.Write("Ingrese un número entre 1 y 10 (0 para salir): ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero >= 1 && numero <= 10)
+            {
+                Console.WriteLine("Tabla de multiplicar del " + numero);
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine(numero + " x " + i + " = " + (numero * i));
+                }
+            }
+            else if (numero != 0)
+            {
+                Console.WriteLine("Por favor, ingrese un número válido.");
+            }
+
+        } while (numero != 0);
+
+        Console.WriteLine("Programa terminado.");
+    }
+}
